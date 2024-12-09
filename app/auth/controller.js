5b14a00e173db3d7f57d32daf8ef26166aab4ff8
Fauzan Husain <achmadfauzan1340@gmail.com>
2024-12-09 14:46:49 +0800

    fixing email

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -7,34 +7,20 @@ const { jwtkey } = require("../../config")
 module.exports = {
     register: async(req, res) => {
         try {
-            const { username, province, email, password } = req.body
+            const { username, province, password } = req.body
 
-            // cek valid email
-            const isEmail = validator.isEmail(email)
-            if(isEmail) {
-                // cek duplikat email
-                const existingEmail = await User.findOne({ email })
-                if (existingEmail) {
-                    return res.status(400).json({ message: `email ${email} sudah terdaftar!` });
-                }
-    
-                // cek duplikat username
-                const existingUsername = await User.findOne({ username });
-                if (existingUsername) {
-                    return res.status(400).json({ message: `username ${username} sudah terdaftar!` })
-                }
-
-                const newUser = new User({
-                    username,
-                    province,
-                    email,
-                    password,
-                })
-                const savedUser = await newUser.save()
-                res.status(201).json({ data: savedUser })
-            } else {
-                return res.status(400).json({ message: `${email} sudah digunakan!` })
+            const existingUsername = await User.findOne({ username });
+            if (existingUsername) {
+                return res.status(400).json({ message: `username ${username} sudah terdaftar!` })
             }
+
+            const newUser = new User({
+                username,
+                province,
+                password,
+            })
+            const savedUser = await newUser.save()
+            res.status(201).json({ data: savedUser })
         } catch (err) {
             res.status(500).json({ err: err.message || "Internal server error" })
         }
--- app/user/model.js
+++ app/user/model.js
@@ -14,6 +14,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        default: "",
         unique: true
     },
     password: {
